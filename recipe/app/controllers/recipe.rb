767f8ccfa62b962f4a22eb60c8cb42ff05b710d1
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-04-14 18:17:36 -0400

    adding flash message if recipe not found through a user search

--- recipe/app/controllers/recipe.rb
+++ recipe/app/controllers/recipe.rb
@@ -1,19 +1,34 @@
 RecipeApp::App.controllers :recipe do
   
   get :index do
-    @recipes = Recipe.all	
-
+    @recipes = Recipe.all
+    @categories = Category.all 	
     render "recipe/index"
 
   end
 
- get :show, :map => 'recipe/ingredients/:name' do
- 	#this first gets it out of the array.	
- 	@recipe = Recipe.where("name = ?", params[:name]).first
- 	@ingredients = @recipe.ingredients
+ 	get :show, :map => 'recipe/ingredients/:name' do
+	 	@recipe = Recipe.where("name = ?", params[:name]).first
+	 	@ingredients = @recipe.ingredients
+		render 'recipe/show'
+  end
 
-  render 'recipe/show'
- end
+  post :show do
+
+	 	if Recipe.where("name = ?", params[:name]).first != nil
+
+		 	@recipe = Recipe.where("name = ?", params[:name]).first
+		 	@ingredients = @recipe.ingredients
+	  	render 'recipe/show'
+	  
+	  else
+
+	  	flash[:notice] = "Sorry recipe not found"
+	  	redirect(url(:recipe, :index))
+	  
+	  end
+
+  end
 
 
 
